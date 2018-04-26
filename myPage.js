f9de2125e032e73a901bacf69c919fadd8dfc74b
Tiril Grimsrud <tiril@desktop-fb22tc8.kea.local>
2018-04-26 10:54:47 +0200

    removed some unessecary css and myPage removed from file

--- myPage.js
+++
@@ -1,6 +0,0 @@
-// url
-
-let urlParams = new URLSearchParams(window.location.search);
-let userName = urlParams.get("name");
-let userHobby = urlParams.get("hobby");
-console.log(userName + " has a hobby, and it's " + userHobby);
